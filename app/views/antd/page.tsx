a4aaf6f6c4b67d4fb82e3e80c6242706fdc7bca1
chengshengwen <chenshengwen@lancode.com>
2023-10-23 11:50:39 +0800

    init husky

--- app/views/antd/page.tsx
+++ app/views/antd/page.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import { Button } from 'antd';
 
-const HomePage = () => (
+const HomePage = () = (
   <div className="App">
     <Button type="primary">Button</Button>
   </div>
