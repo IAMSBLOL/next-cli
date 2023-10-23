5b8ca1843db3aae39e20bc1c57d43b8b9c2688d5
chengshengwen <chenshengwen@lancode.com>
2023-10-23 11:46:07 +0800

    init antd

---
+++ app/views/antd/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import React from 'react';
+import { Button } from 'antd';
+
+const HomePage = () => (
+  <div className="App">
+    <Button type="primary">Button</Button>
+  </div>
+);
+
+export default HomePage;
