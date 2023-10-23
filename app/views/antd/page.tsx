e91681ff0ee98973732a81756c25b21cf04c62e8
chengshengwen <chenshengwen@lancode.com>
2023-10-23 11:52:05 +0800

    init husky text

--- app/views/antd/page.tsx
+++ app/views/antd/page.tsx
@@ -3,9 +3,10 @@
 import React from 'react';
 import { Button } from 'antd';
 
-const HomePage = () = (
+const HomePage1 = () => (
   <div className="App">
-    <Button type="primary">Button</Button>
+    <Button
+      type="primary">Button</Button>
   </div>
 );
 
